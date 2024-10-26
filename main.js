230a7761eb2beec29df0e30a881fa92165d56bbb
Daggerz23 <aleksandrgarnovich10005@gmail.com>
2024-10-26 15:44:41 +0300

    test

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelectorAll(".projects-block > div > video").forEach((elem) => {
+    elem.addEventListener("mouseover", function() {
+        this.play()
+    });
+})
+
+document.querySelectorAll(".projects-block > div > video").forEach((elem) => {
+    elem.addEventListener("mouseleave", function() {
+        this.currentTime = 0;
+        this.pause()
+    });
+})
